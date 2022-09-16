1a230c847ec43f9ab8ad8c0fb9cba363b3c1c57e
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-09-16 11:15:28 +0530

    it is udemy exercise.

--- src/Printer.java
+++ src/Printer.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Printer {
+public class Printer {
+    private int tonerLevel = -1;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        if (tonerLevel > -1 && tonerLevel <= 100)
+            this.tonerLevel = tonerLevel;
+        this.pagesPrinted = 0;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        if (tonerAmount > 0 && tonerAmount <= 100) {
+            if (tonerLevel + tonerAmount > 100) {
+                return -1;
+            }
+            tonerLevel += tonerAmount;
+            return tonerLevel;
+        } else
+            return -1;
+
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (duplex) {
+            pagesToPrint = (pages / 2) + (pages % 2);
+            System.out.println("Printing in duplex mode");
+        }
+        this.pagesPrinted += pagesToPrint;
+
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
 }
